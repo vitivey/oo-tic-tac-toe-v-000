86c03b3ac96da6d8db92a9169dcb2438f29cdbff
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-05 20:59:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,6 +3,17 @@ class TicTacToe
     @board =  Array.new(9," ")
   end
 
+WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
 def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
@@ -56,17 +67,7 @@ end
 
 def won?
     winning_combination=[]
-    @WIN_COMBINATIONS = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ]
-    @WIN_COMBINATIONS.each do |win_option|
+    WIN_COMBINATIONS.each do |win_option|
     win_index_1 = win_option[0]
     win_index_2 = win_option[1]
     win_index_3 = win_option[2]
