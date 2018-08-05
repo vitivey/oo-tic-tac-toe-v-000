98360f9bb8a184e7aca2c1c527bc28c8e89e112d
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-05 21:38:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,7 @@ class TicTacToe
   end
 
   def position_taken?(index)
-    !(@board[@index].nil? || @board[@index] == " ")
+    !(@board[index].nil? || @board[index] == " ")
   end
 
   def valid_move?(index)
